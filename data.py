998bcc758f4f6b76f761c4dc07fecaa804648854
Andy@911 <decemberandile@gmail.com>
2021-04-10 01:25:41 +0200

    connected to the postgresql database

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'json data is an array of arrays',
+            'author': 'Andile Gumada',
+            'create_date': '09-04-2021'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'json data is an array of arrays',
+            'author': 'Andile Gumada',
+            'create_date': '09-04-2021'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'json data is an array of arrays',
+            'author': 'Andile Gumada',
+            'create_date': '09-04-2021'
+        }
+    ]
+    return articles
